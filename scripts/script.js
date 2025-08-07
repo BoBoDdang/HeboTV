d53088f8888aee3aca3642e9a41e8051aaee7bae
Hyunwoo Choi <chwoo.dev@gmail.com>
2025-08-07 22:07:59 +0900

    update

--- scripts/script.js
+++ scripts/script.js
@@ -4,13 +4,13 @@ const video = document.getElementById('video');
 let volumeFade = 0;
 let muted = localStorage.getItem('muted') == 'true';
 let volume = parseFloat(localStorage.getItem('volume'));
-if (volume == NaN) volume = 0.8;
 
 let streamData;
 
 (async () => {
+    if (volume == NaN) volume = 0.8;
     $('volume-range').value = volume * 100 * +!muted;
-    volumeRange.style.setProperty('--val', `${volume * 100* +!muted}%`);
+    volumeRange.style.setProperty('--val', `${volume * 100 * +!muted}%`);
     if (volume == 0) muted = true;
     changeIcon(volumeButton, muted ? 'e710' : 'e050');
 
